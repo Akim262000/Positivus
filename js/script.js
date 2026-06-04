4c8a71cff1ccde6cb9aaef8644e658f5d5fa990e
Akim Minko <akim.minko@yandex.ru>
2026-06-04 16:24:08 +0300

    feat: add new sections

---
+++ js/script.js
@@ -0,0 +1,55 @@
+document.querySelectorAll(".collapse").forEach((collapse) => {
+  collapse.addEventListener("show.bs.collapse", function () {
+    const card = this.closest(".working-process-card");
+    const button = card.querySelector(".process-button");
+    const icon = button.querySelector(".pm-icon");
+
+    card.classList.remove("bg-grey");
+    card.classList.add("bg-lime");
+
+    icon.src = "/images/minus.png";
+  });
+
+  collapse.addEventListener("hide.bs.collapse", function () {
+    const card = this.closest(".working-process-card");
+    const button = card.querySelector(".process-button");
+    const icon = button.querySelector(".pm-icon");
+
+    card.classList.remove("bg-lime");
+    card.classList.add("bg-grey");
+
+    icon.src = "/images/plus.png";
+  });
+});
+
+$(".testimonials-slider").slick({
+  slidesToShow: 1,
+  variableWidth: true,
+  centerMode: true,
+  arrows: true,
+  dots: true,
+  appendDots: $(".custom-dots"),
+
+  prevArrow: $(".prev-arrow"),
+  nextArrow: $(".next-arrow"),
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
